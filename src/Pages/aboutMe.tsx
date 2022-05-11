003de04df0b1507879abe197f14b9849908aa6d3
John Collandra <jpcollandra@gmail.com>
2022-05-11 14:33:40 -0400

    added bio

--- src/Pages/aboutMe.tsx
+++ src/Pages/aboutMe.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Col from 'react-bootstrap/esm/Col'
-import Container from 'react-bootstrap/esm/Container'
-import Row from 'react-bootstrap/esm/Row'
+import React from "react";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
 
 export default function AboutMe() {
   return (
     <>
-    <Container
+      <Container
         fluid
         className="p-0"
         style={{
@@ -17,13 +17,23 @@ export default function AboutMe() {
           width: "100%",
         }}
       >
-      <Row>
-        <Col style={{position: "relative", justifyContent: "center"}}>
-
-        <h2 > Hello </h2>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col>
+            <div style={{position: "absolute", right: "30%", left: "20%", width: "600px", marginTop: "15%"}}>
+              <h2>
+                <span> Hello World</span>
+                <span style={{ fontSize: "24px" }}>
+                  , my name is John. I am the founder of Negnan.com, a
+                  revolutionary way of understanding current events. I also do
+                  freelance sofware development to supplement my income. If you
+                  have an idea you want to work on, or just want to say hi,
+                  please get in touch.
+                </span>
+              </h2>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
